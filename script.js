e7693908a47885409a92edfd7405e42a6bfa3eff
ferruhcihan <ferruhcihan20@gmail.com>
2020-11-14 21:08:03 +0300

    script updated

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ const JS30Data = [
     //     id: 4,
     //     title: "Array Cardio Day 1",
     //     imgURL: "./images/css-variables.png",
-    //     repoURL: "https://github.com/ferruhcihan/JS30-04-Array-Cardio-Day-1",
-    //     liveURL: "https://ferruhcihan.github.io/JS30-04-Array-Cardio-Day-1/"
+    //     repoURL: "https://github.com/ferruhcihan/JS30-05-Flex-Panel-Gallery",
+    //     liveURL: "https://ferruhcihan.github.io/JS30-05-Flex-Panel-Gallery/"
     // },
     
 ]
